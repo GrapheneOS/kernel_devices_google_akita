453411eb0f85d704fc5d574d4c4d236195ca77c6
Safayat Ullah <safayat@google.com>
2023-05-29 14:55:42 +0000

    panel: fine tune power on sequence for ak3b
    Bug: 283227029
    Test: suspend resume panel
    Signed-off-by: Safayat Ullah <safayat@google.com>
    Change-Id: I1e2e2f6de3e9bdeede532314d59d5c87d1e67268

--- display/panel-google-ak3b.c
+++ display/panel-google-ak3b.c
@@ -759,6 +759,17 @@ const struct exynos_panel_desc google_ak3b = {
 	.no_lhbm_rr_constraints = true,
 	.panel_func = &ak3b_drm_funcs,
 	.exynos_panel_func = &ak3b_exynos_funcs,
+	.reset_timing_ms = {1, 1, 20},
+	.reg_ctrl_enable = {
+		{PANEL_REG_ID_VDDI, 0},
+		{PANEL_REG_ID_VCI, 0},
+		{PANEL_REG_ID_VDDD, 10},
+	},
+	.reg_ctrl_disable = {
+		{PANEL_REG_ID_VDDD, 0},
+		{PANEL_REG_ID_VCI, 0},
+		{PANEL_REG_ID_VDDI, 0},
+	},
 };
 
 static const struct of_device_id exynos_panel_of_match[] = {
